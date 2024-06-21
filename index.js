0741fda2216ba8a308589fce308d5f1d786d2df7
Pat Heard <patrick.heard@cds-snc.ca>
2024-06-21 14:17:50 -0400

    feat: update to confirm flow and add expects

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const playwright = require('playwright-aws-lambda');
+const { expect } = require('@playwright/test');
 
 exports.handler = async (event, context) => {
   let browser = null;
@@ -9,10 +10,18 @@ exports.handler = async (event, context) => {
     const page = await context.newPage();
 
     await page.goto(event.url);
+
+    // Confirm expected form and submit
+    await expect(page.locator('h1')).toHaveText('Healthcheck (email)');
+    await page.waitForTimeout(1000);
+    await page.locator('form#form label[for="1.0"]').click();
+    await page.locator('form#form button[type="button"]').click();
     await page.waitForTimeout(5000);
-    await page.locator('label[for="1.0"]').click();
-    await page.locator('button[type="submit"]').click();
+    await page.locator('form#form button[type="submit"]').click();
+  
+    // Check form has been submitted
     await page.waitForURL('**/confirmation');
+    await expect(page.locator('h1')).toHaveText('Your form has been submitted');
 
   } catch (error) {
     throw error;
